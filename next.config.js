1a0079a673e873626f15ca73fe50b03d09340066
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-27 17:16:26 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com', 'via.placeholder.com'],
-    unoptimized: true,
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
